71b3c75ea563811f0a029cc4b17b9416d43cdda1
mehul <mehulzinzuvadiya098@gmail.com>
2023-04-24 10:52:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'View/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/': (context) => HomeScreen(),
+    },
+  ));
+}
